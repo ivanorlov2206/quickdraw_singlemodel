8fd583d53ef2137a6eeb1cd7f701153c0327f4e0
MineOrpe <codermineor@gmail.com>
2021-02-06 20:02:04 +0300

    Added summarizing of probabilities

--- worker.js
+++ worker.js
@@ -19,19 +19,38 @@ onmessage = async function (event) {
     let tf_arr = tf.tensor2d(arr);
 
     var classes = ['airplane', 'alarm clock', 'ambulance', 'angel', 'animal migration', 'ant', 'anvil', 'apple', 'arm', 'asparagus', 'axe', 'backpack', 'banana', 'bandage', 'barn', 'baseball', 'baseball bat', 'basket', 'basketball', 'bat', 'bathtub', 'beach', 'bear', 'beard', 'bed', 'bee', 'belt', 'bench', 'bicycle', 'binoculars', 'bird', 'birthday cake', 'blackberry', 'blueberry', 'book', 'boomerang', 'bottlecap', 'bowtie', 'bracelet', 'brain', 'bread', 'bridge', 'broccoli', 'broom', 'bucket', 'bulldozer', 'bus', 'bush', 'butterfly', 'cactus', 'cake', 'calculator', 'calendar', 'camel', 'camera', 'camouflage', 'campfire', 'candle', 'cannon', 'canoe', 'car', 'carrot', 'castle', 'cat', 'ceiling fan', 'cell phone', 'cello', 'chair', 'chandelier', 'church', 'circle', 'clarinet', 'clock', 'cloud', 'coffee cup', 'compass', 'computer', 'cookie', 'cooler', 'couch', 'cow', 'crab', 'crayon', 'crocodile', 'crown', 'cruise ship', 'cup', 'diamond', 'dishwasher', 'diving board', 'dog', 'dolphin', 'donut', 'door', 'dragon', 'dresser', 'drill', 'drums', 'duck', 'dumbbell', 'ear', 'elbow', 'elephant', 'envelope', 'eraser', 'eye', 'eyeglasses', 'face', 'fan', 'feather', 'fence', 'finger', 'fire hydrant', 'fireplace', 'firetruck', 'fish', 'flamingo', 'flashlight', 'flip flops', 'floor lamp', 'flower', 'flying saucer', 'foot', 'fork', 'frog', 'frying pan', 'garden', 'garden hose', 'giraffe', 'goatee', 'golf club', 'grapes', 'grass', 'guitar', 'hamburger', 'hammer', 'hand', 'harp', 'hat', 'headphones', 'hedgehog', 'helicopter', 'helmet', 'hexagon', 'hockey puck', 'hockey stick', 'horse', 'hospital', 'hot air balloon', 'hot dog', 'hot tub', 'hourglass', 'house', 'house plant', 'hurricane', 'ice cream', 'jacket', 'jail', 'kangaroo', 'key', 'keyboard', 'knee', 'ladder', 'lantern', 'laptop', 'leaf', 'leg', 'light bulb', 'lighthouse', 'lightning', 'line', 'lion', 'lipstick', 'lobster', 'lollipop', 'mailbox', 'map', 'marker', 'matches', 'megaphone', 'mermaid', 'microphone', 'microwave', 'monkey', 'moon', 'mosquito', 'motorbike', 'mountain', 'mouse', 'moustache', 'mouth', 'mug', 'mushroom', 'nail', 'necklace', 'nose', 'ocean', 'octagon', 'octopus', 'onion', 'oven', 'owl', 'paint can', 'paintbrush', 'palm tree', 'panda', 'pants', 'paper clip', 'parachute', 'parrot', 'passport', 'peanut', 'pear', 'peas', 'pencil', 'penguin', 'piano', 'pickup truck', 'picture frame', 'pig', 'pillow', 'pineapple', 'pizza', 'pliers', 'police car', 'pond', 'pool', 'popsicle', 'postcard', 'potato', 'power outlet', 'purse', 'rabbit', 'raccoon', 'radio', 'rain', 'rainbow', 'rake', 'remote control', 'rhinoceros', 'river', 'roller coaster', 'rollerskates', 'sailboat', 'sandwich', 'saw', 'saxophone', 'school bus', 'scissors', 'scorpion', 'screwdriver', 'sea turtle', 'see saw', 'shark', 'sheep', 'shoe', 'shorts', 'shovel', 'sink', 'skateboard', 'skull', 'skyscraper', 'sleeping bag', 'smiley face', 'snail', 'snake', 'snorkel', 'snowflake', 'snowman', 'soccer ball', 'sock', 'speedboat', 'spider', 'spoon', 'spreadsheet', 'square', 'squiggle', 'squirrel', 'stairs', 'star', 'steak', 'stereo', 'stethoscope', 'stitches', 'stop sign', 'stove', 'strawberry', 'streetlight', 'string bean', 'submarine', 'suitcase', 'sun', 'swan', 'sweater', 'swing set', 'sword', 't-shirt', 'table', 'teapot', 'teddy-bear', 'telephone', 'television', 'tennis racquet', 'tent', 'the eiffel tower', 'the great wall of china', 'the mona lisa', 'tiger', 'toaster', 'toe', 'toilet', 'tooth', 'toothbrush', 'toothpaste', 'tornado', 'tractor', 'traffic light', 'train', 'tree', 'triangle', 'trombone', 'truck', 'trumpet', 'umbrella', 'underwear', 'van', 'vase', 'violin', 'washing machine', 'watermelon', 'waterslide', 'whale', 'wheel', 'windmill', 'wine bottle', 'wine glass', 'wristwatch', 'yoga', 'zebra', 'zigzag'];
+    var summing = {"axe": ['hammer', 'mailbox'], "bear": ["panda", "lion", "dog"], "door": ["cooler"], "tree": ['broccoli'], "hat": ["flying saucer"], "camera": ["washing machine"],
+     "lollipop": ["popsicle"], "house": ["barn"], "traffic light": ["streetlight"], "flower": ["windmill"], "shorts": ["pants"], "stairs": ["ladder"],
+   "zigzag": ["lightning"], "butterfly": ["bat"], "cloud": ["bush"], "crown": ["grass"], "hockey stick": ["golf club", "clarinet"], "hand": ["arm"], "sock": ["foot", "shoe"]};
     let offset = tf.scalar(127.5);
     tf_arr = tf_arr.sub(offset).div(offset).reshape([-1, 64, 64, 1]);
 
-    const {values, indices} = tf.topk(model.predict(tf_arr), 5);
+    var predicted = model.predict(tf_arr).dataSync();
+    var predi = [];
+    for (var i = 0; i < predicted.length; i++){
+      var clname = classes[i];
+      var rsum = 0;
+      if (clname in summing) {
+        for (var j = 0; j < summing[clname].length; j++) {
+          rsum += predicted[classes.indexOf(summing[clname][j])];
+        }
+      }
+      console.log("Summing to " + classes[i] + " " + rsum + " " + predicted[i]);
+      predi[i] = predicted[i] + rsum;
+    }
+    var cT = tf.tensor(predi);
+    const {values, indices} = tf.topk(cT, 5);
     var ind_js = Array.from(indices.dataSync());
     var val_js = Array.from(values.dataSync());
     var resarr = {};
     var sum = 0;
+
     ind_js.map(function(e, i) {
       sum += val_js[i];
+
     });
     ind_js.map(function(e, i) {
-      resarr[classes[ind_js[i]]] = val_js[i] / sum;
+      resarr[classes[ind_js[i]]] = val_js[i];
     });
     console.log(JSON.stringify(resarr));
     postMessage("predres;;;" + JSON.stringify(resarr));
